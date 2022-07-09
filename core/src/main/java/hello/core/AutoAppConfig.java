93523b5a6c510edc8f9d252103c3756683a84a5f
younglaecho <tks7205@gmail.com>
2022-07-09 23:26:47 +0900

    learn spring component scan

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class, // default : hello.core 아래의 모든 패키지
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository () {
+        return new MemoryMemberRepository();
+    }
+
+}
